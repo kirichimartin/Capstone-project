2ec981e80da4901315fe7ee2770540346be85c11
kirichimartin <147861416+kirichimartin@users.noreply.github.com>
2023-11-28 14:39:04 +0300

    new Update project.c

--- project.c
+++ project.c
@@ -4,26 +4,32 @@
 
 #define MAX_SEATS 50
 
+// User structure to store credentials
 struct User {
     char id[20];
     char password[15];
 };
 
+// Reservation structure to represent a seat reservation
 struct Reservation {
     int seat_number;
     char passenger_name[20];
     int is_reserved;
 };
 
+// Bus structure to store information about a bus
 struct Bus {
     int busNumber;
     char busName[15];
     char source[20];
     char destination[20];
+    char departure_time[10];
+    char arrival_time[10];
     float price;
     struct Reservation seats[MAX_SEATS];
 };
 
+// Database of users
 struct User database[] = {
     {"manager", "@manager20"},
     {"supervisor", "supervisor@23"},
@@ -84,9 +90,12 @@ int main() {
         printf("Login failed. Invalid ID or password.\n");
     }
 
+    saveDataToFile(); // Save the data to file before exiting
+
     return 0;
 }
 
+// Function to search for a user's credentials in the database
 int searchUser(const struct User* database, int size, const char* enteredId, const char* enteredPassword) {
     int i;
     for (i = 0; i < size; i++) {
@@ -97,6 +106,7 @@ int searchUser(const struct User* database, int size, const char* enteredId, con
     return 0; // Login failed
 }
 
+// Function to display the main menu and get user's choice
 int displayMenu() {
     int choice;
     printf("Choose an option:\n");
@@ -107,6 +117,7 @@ int displayMenu() {
     return choice;
 }
 
+// Function to initialize bus reservations
 void initializeBus(struct Bus* bus) {
     int i;
     for (i = 0; i < MAX_SEATS; i++) {
@@ -116,6 +127,7 @@ void initializeBus(struct Bus* bus) {
     }
 }
 
+// Function to display information about bus seats
 void displayBus(const struct Bus* bus) {
     printf("Seat Number\tPassenger Name\n");
     int i;
@@ -124,6 +136,7 @@ void displayBus(const struct Bus* bus) {
     }
 }
 
+// Function to reserve a seat on a bus
 void reserveSeat(struct Bus* bus) {
     int seat;
     printf("Enter the seat number you want to reserve: ");
@@ -144,6 +157,7 @@ void reserveSeat(struct Bus* bus) {
     }
 }
 
+// Function to manage bus information
 void busInformationManagement() {
     int busChoice;
     do {
@@ -186,6 +200,7 @@ void busInformationManagement() {
     } while (busChoice != 0);
 }
 
+// Function to add a new bus
 void addBus() {
     if (totalBuses < 10) {
         printf("Enter bus number: ");
@@ -196,6 +211,10 @@ void addBus() {
         scanf("%s", buses[totalBuses].source);
         printf("Enter destination: ");
         scanf("%s", buses[totalBuses].destination);
+        printf("Enter departure time: ");
+        scanf("%s", buses[totalBuses].departure_time);
+        printf("Enter arrival time: ");
+        scanf("%s", buses[totalBuses].arrival_time);
         printf("Enter price: ");
         scanf("%f", &buses[totalBuses].price);
 
@@ -209,6 +228,7 @@ void addBus() {
     }
 }
 
+// Function to delete a bus
 void deleteBus(int busNumber) {
     int i, found = 0;
 
@@ -227,6 +247,7 @@ void deleteBus(int busNumber) {
     }
 }
 
+// Function to modify bus information
 void modifyBus(int busNumber) {
     int i, found = 0;
 
@@ -245,18 +266,22 @@ void modifyBus(int busNumber) {
     }
 }
 
+// Function to view all available buses
 void viewBuses() {
     if (totalBuses == 0) {
         printf("No buses available!\n");
     } else {
-        printf("Bus Number\tBus Name\tSource\t\tDestination\tPrice\n");
+        printf("Bus Number\tBus Name\tSource\t\tDestination\tDeparture Time\tArrival Time\tPrice\n");
         int i;
         for (i = 0; i < totalBuses; i++) {
-            printf("%d\t\t%s\t\t%s\t\t%s\t\t%.2f\n", buses[i].busNumber, buses[i].busName, buses[i].source, buses[i].destination, buses[i].price);
+            printf("%d\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%.2f\n", buses[i].busNumber, buses[i].busName,
+                   buses[i].source, buses[i].destination, buses[i].departure_time, buses[i].arrival_time,
+                   buses[i].price);
         }
     }
 }
 
+// Function to save data to a file
 void saveDataToFile() {
     FILE* file = fopen("data.txt", "w");
     if (file == NULL) {
@@ -267,12 +292,14 @@ void saveDataToFile() {
     fprintf(file, "%d\n", totalBuses);
     int i;
     for (i = 0; i < totalBuses; ++i) {
-        fprintf(file, "%d %s %s %s %f\n", buses[i].busNumber, buses[i].busName, buses[i].source, buses[i].destination, buses[i].price);
+        fprintf(file, "%d %s %s %s %s %s %f\n", buses[i].busNumber, buses[i].busName, buses[i].source,
+                buses[i].destination, buses[i].departure_time, buses[i].arrival_time, buses[i].price);
     }
 
     fclose(file);
 }
 
+// Function to read data from a file
 void readDataFromFile() {
     FILE* file = fopen("data.txt", "r");
     if (file == NULL) {
@@ -283,7 +310,8 @@ void readDataFromFile() {
     fscanf(file, "%d", &totalBuses);
     int i;
     for (i = 0; i < totalBuses; ++i) {
-        fscanf(file, "%d %s %s %s %f", &buses[i].busNumber, buses[i].busName, buses[i].source, buses[i].destination, &buses[i].price);
+        fscanf(file, "%d %s %s %s %s %s %f", &buses[i].busNumber, buses[i].busName, buses[i].source,
+               buses[i].destination, buses[i].departure_time, buses[i].arrival_time, &buses[i].price);
         initializeBus(&buses[i]);
     }
 
